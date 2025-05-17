b21cd50306759b0f62b14f8b1a215cfb29609cbe
Edinson Rivera <edirju@gmail.com>
2025-05-17 13:36:49 -0500

    add projects

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -54,155 +54,144 @@ export const PROJECTS = [
     },
     youtube: "https://www.youtube.com/watch?v=157qVlTelOg",
   },
-  // {
-  //     slug: "06-pruebas",
-  //     title: "Pruebas CSS + JS",
-  //     description:
-  //       "Aplicación clásica para dibujar píxeles con un color y un tamaño",
-  //     learnings: ["Dibujar en Canvas", "Function", "Eventos de teclado", "Touch"],
-  //     theme: {
-  //     isDark: true,
-  //   },
-  //     youtube: "https://www.youtube.com/watch?v=b6du6MvQmuQ",
-  //   },
-  // {
-  //   slug: "06-pixel-art-generator",
-  //   title: "Pixel Art Generator",
-  //   description:
-  //     "Aplicación clásica para dibujar píxeles con un color y un tamaño",
-  //   learnings: ["Dibujar en Canvas", "Function", "Eventos de teclado", "Touch"],
-  //   youtube: "https://www.youtube.com/watch?v=b6du6MvQmuQ",
-  // },
-  // {
-  //   slug: "07-rich-text-editor",
-  //   title: "Editor Online",
-  //   description: "Editor similar a Word, pero más pequeño",
-  //   learnings: ["Custom CSS", "Functions", "ESModules"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "08-password-generator",
-  //   title: "Crea contraseñas",
-  //   description: "Generador de contraseñas de forma segura",
-  //   learnings: ["Custom CSS", "Functions", "ESModules"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "09-qr-code-generator",
-  //   title: "Crea códigos QR",
-  //   description: "Generador de códigos QR en JS",
-  //   learnings: ["Custom CSS", "Functions", "ESModules"],
-  //   theme: {
-  //     isDark: true,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "10-adivinanza-juego",
-  //   title: "Adivina el número",
-  //   description: "Juego que nos permite adivinar un número",
-  //   learnings: ["Custom CSS", "Funciones", "Condicionales"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "11-calculadora-v1",
-  //   title: "Calculadora JS",
-  //   description: "calculadora con funciones aritméticas básicas.",
-  //   learnings: ["Custom CSS", "Funciones", "Condicionales"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "12-movie-seat-booking",
-  //   title: "Reserva de asientos cine",
-  //   description:
-  //     "Simulador que permite asignar asientos de cine a los usuarios.",
-  //   learnings: ["Custom CSS", "Funciones", "Condicionales"],
-  //   theme: {
-  //     isDark: true,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "13-exchange-rate",
-  //   title: "Conversor de monedas",
-  //   description:
-  //     "Aplicacion que nos permite calcular el valor de una moneda en otra.",
-  //   learnings: ["Custom CSS", "Funciones", "Condicionales"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "14-hangman",
-  //   title: "Juego El Ahorcado",
-  //   description:
-  //     "Aplicacion que nos permite calcular el valor de una moneda en otra.",
-  //   learnings: ["Custom CSS", "Funciones", "Condicionales"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "15-arkanoid-game",
-  //   title: "Arkanoid",
-  //   description:
-  //     "Juego mítico y clásico de Arkanoid para controlar con teclado.",
-  //   learnings: [
-  //     "Dibujar en Canvas",
-  //     "Eventos de teclado",
-  //     "RequestAnimationFrame",
-  //     "Sprites",
-  //   ],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "16-breakout-game",
-  //   title: "Breakout",
-  //   description:
-  //     "Juego clásico de Breakout para controlar con teclado y mouse.",
-  //   learnings: ["Dibujar en Canvas", "Eventos de teclado", "Clases"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "17-chatgpt-local",
-  //   title: "ChatGPT local",
-  //   description: "Usa IA de forma local y gratis. 100% privacidad.",
-  //   learnings: ["Web Workers", "IA", "ESModules"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
-  // {
-  //   slug: "18-efectos-css",
-  //   title: "Efectos CSS",
-  //   description: "Espectaculares efectos en CSS para tus proyectos.",
-  //   learnings: ["animaciones", "CSS", "Clases"],
-  //   theme: {
-  //     isDark: false,
-  //   },
-  //   youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
-  // },
+  {
+    slug: "06-pixel-art-generator",
+    title: "Pixel Art Generator",
+    description:
+      "Aplicación clásica para dibujar píxeles con un color y un tamaño",
+    learnings: ["Dibujar en Canvas", "Function", "Eventos de teclado", "Touch"],
+    youtube: "https://www.youtube.com/watch?v=b6du6MvQmuQ",
+  },
+  {
+    slug: "07-rich-text-editor",
+    title: "Editor Online",
+    description: "Editor similar a Word, pero más pequeño",
+    learnings: ["Custom CSS", "Functions", "ESModules"],
+    theme: {
+      isDark: false,
+    },
+     youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "08-password-generator",
+    title: "Crea contraseñas",
+    description: "Generador de contraseñas de forma segura",
+    learnings: ["Custom CSS", "Functions", "ESModules"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "09-qr-code-generator",
+    title: "Crea códigos QR",
+    description: "Generador de códigos QR en JS",
+    learnings: ["Custom CSS", "Functions", "ESModules"],
+    theme: {
+      isDark: true,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "10-adivinanza-juego",
+    title: "Adivina el número",
+    description: "Juego que nos permite adivinar un número",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "11-calculadora-v1",
+    title: "Calculadora JS",
+    description: "calculadora con funciones aritméticas básicas.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "12-movie-seat-booking",
+    title: "Reserva de asientos cine",
+    description:
+      "Simulador que permite asignar asientos de cine a los usuarios.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: true,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "13-exchange-rate",
+    title: "Conversor de monedas",
+    description:
+      "Aplicacion que nos permite calcular el valor de una moneda en otra.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "14-hangman",
+    title: "Juego El Ahorcado",
+    description:
+      "Aplicacion que nos permite calcular el valor de una moneda en otra.",
+    learnings: ["Custom CSS", "Funciones", "Condicionales"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "15-arkanoid-game",
+    title: "Arkanoid",
+    description:
+      "Juego mítico y clásico de Arkanoid para controlar con teclado.",
+    learnings: [
+      "Dibujar en Canvas",
+      "Eventos de teclado",
+      "RequestAnimationFrame",
+      "Sprites",
+    ],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "16-breakout-game",
+    title: "Breakout",
+    description:
+      "Juego clásico de Breakout para controlar con teclado y mouse.",
+    learnings: ["Dibujar en Canvas", "Eventos de teclado", "Clases"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "17-chatgpt-local",
+    title: "ChatGPT local",
+    description: "Usa IA de forma local y gratis. 100% privacidad.",
+    learnings: ["Web Workers", "IA", "ESModules"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
+  {
+    slug: "18-efectos-css",
+    title: "Efectos CSS",
+    description: "Espectaculares efectos en CSS para tus proyectos.",
+    learnings: ["animaciones", "CSS", "Clases"],
+    theme: {
+      isDark: false,
+    },
+    youtube: "https://www.youtube.com/watch?v=HvoiF1MCPGs",
+  },
   // {
   //   slug: "19-css-hover",
   //   title: "CSS Hover + Animación",
